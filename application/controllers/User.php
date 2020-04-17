c335da5b82834ddefaca41ec76432847281b5d82
edbert27 <edbertandoyo27@gmail.com>
2020-04-17 09:00:26 +0700

    update profile

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -21,4 +21,51 @@ class User extends CI_Controller
         $this->load->view("user/index", $data);
         $this->load->view("templates/footer");
     }
+
+    public function edit()
+    {
+        $data['title'] = "Edit Profile";
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view("templates/header", $data);
+            $this->load->view("templates/sidebar", $data);
+            $this->load->view("templates/topbar", $data);
+            $this->load->view("user/edit", $data);
+            $this->load->view("templates/footer");
+        } else {
+
+            $name = $this->input->post('name');
+            $upload_image = $_FILES['image']['name'];
+
+            if ($upload_image) {
+                $config['allowed_types'] = 'gif|jpg|png';
+                $config['max_sizes'] = '2048';
+                $config['upload_path'] = './assets/img/profile/';
+
+                $this->load->library('upload', $config);
+
+                if ($this->upload->do_upload('image')) {
+                    $old_image = $data['user']['image'];
+
+                    if ($old_image != 'default.png') {
+                        unlink(FCPATH . 'assets/img/profile/' . $old_image);
+                    }
+                    $new_image = $this->upload->data('file_name');
+                    $this->db->set('image', $new_image);
+                } else {
+                    echo $this->upload->display_errors();
+                }
+            }
+
+            $this->db->set('name', $name);
+            $this->db->where('id', $data['user']['id']);
+            $this->db->update('user');
+
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Profile has been updated!<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
+            redirect('user');
+        }
+    }
 }
